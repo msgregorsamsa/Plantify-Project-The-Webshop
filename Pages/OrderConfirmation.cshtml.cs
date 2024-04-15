d495f3d24d0b505aad8fe50468a3478b42fcd062
Sanna Wiklund <sanna.wiklund98@gmail.com>
2024-04-15 20:22:20 +0200

    Added ordernumber with confirmation

--- Pages/OrderConfirmation.cshtml.cs
+++ Pages/OrderConfirmation.cshtml.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Plantify_Project_The_Webshop.Data;
 using Plantify_Project_The_Webshop.Models;
+using System;
+using System.Collections.Generic;
 
 namespace Plantify_Project_The_Webshop.Pages
 {
@@ -11,6 +13,8 @@ namespace Plantify_Project_The_Webshop.Pages
         public List<Cart> Carts { get; set; }
         public Product Product { get; set; }
         public double TotalPrice { get; set; }
+        public int OrderNumber { get; set; }
+
         //Databas
         private readonly AppDbContext database;
         private readonly AccessControl accessControl;
@@ -21,11 +25,17 @@ namespace Plantify_Project_The_Webshop.Pages
             this.accessControl = accessControl;
         }
 
+        static int GenerateOrdernummer(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max + 1);
+        }
+
         public void OnGet(double totalPrice)
         {
             TotalPrice = totalPrice;
+            OrderNumber = GenerateOrdernummer(24050, 25670);
         }
 
     }
 }
-
